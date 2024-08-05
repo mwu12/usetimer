f25d95ad4a419b75fcac81b6298019a85ed79c45
Matt Wu <yhwu999@gmail.com>
2024-08-05 16:20:53 +0800

    timer1 and timer2 are created but timer1 can stop/refresh timer2.
    timer2 does not work if timer1 stops timer2.
    
    timer2 is a THREAD.
    As long as its.it_value.tv_sec is non zero, timer2 is running periodically.
    If not setting tv_sec, the timer will be one time running.

--- main.c
+++ main.c
@@ -6,24 +6,63 @@
 #include <pthread.h>
 
 #define INIT_SEC 3
-#define INTV_SEC 10
+#define INTV_SEC 3
+#define INIT_SEC2 4
+#define INTV_SEC2 4
 
 typedef void (*timer_timeout_callback)(union sigval);
+static timer_t g_timer2;
 static int g_count = 0;
 static void TimeoutAction(union sigval)
 {
     if (g_count == 0)
-        printf("[Thread] Timer expired %d times (%d sec)\n", ++g_count, INIT_SEC);
+        printf("[Thread] Timer expired %d times (%d sec)\n", ++g_count, INIT_SEC2);
     else
-        printf("[Thread] Timer expired %d times (%d sec)\n", ++g_count, INTV_SEC);
+        printf("[Thread] Timer expired %d times (%d sec)\n", ++g_count, INTV_SEC2);
     // Add your status and variable checks here
 }
 
 void timer_handler(int signum)
 {
     static int count = 0;
-    printf("Timer expired %d times\n", ++count);
-    // Add your status and variable checks here
+    struct itimerspec stop_its = {{0,0},{0,0}};
+    struct itimerspec refresh_its = {{INTV_SEC2,0},{INIT_SEC2,0}};
+    int ret = 0;
+
+    if (g_count == 0)
+        printf("Timer expired %d times (%d sec)\n", ++count, INIT_SEC);
+    else
+        printf("Timer expired %d times (%d sec)\n", ++count, INTV_SEC);
+
+    if (count % 3 == 0)
+    {
+        ret = timer_settime(g_timer2, 0, &stop_its, NULL);
+        if (ret != 0)
+        {
+            perror("Fail to stop timer2 !!");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            perror("Succeed to stop timer2 !!");
+            //exit(EXIT_FAILURE);
+        }
+    }
+    if (count % 4 == 0)
+    {
+        ret = timer_settime(g_timer2, 0, &refresh_its, NULL);
+        if (ret != 0)
+        {
+            perror("Fail to refresh timer2 !!");
+            exit(EXIT_FAILURE);
+        }
+        else
+        {
+            perror("Succeed to refresh timer2 !!");
+            //exit(EXIT_FAILURE);
+        }
+    }
+
 }
 
 int main()
@@ -45,8 +84,7 @@ int main()
         exit(EXIT_FAILURE);
     }
 
-    // Set up timer
-#if 0
+    // Set up timer 1
     sev.sigev_notify = SIGEV_SIGNAL;
     sev.sigev_signo = SIGRTMIN;
     sev.sigev_value.sival_ptr = &timerid;
@@ -55,26 +93,31 @@ int main()
         perror("timer_create SIGEV_SIGNAL");
         exit(EXIT_FAILURE);
     }
-#else
+
+    // Set up timer 2
     pthread_attr_init(&attr);
     sev.sigev_notify = SIGEV_THREAD;
     sev.sigev_notify_function = t_callback;
     sev.sigev_notify_attributes = &attr;
-    if (timer_create(CLOCK_REALTIME, &sev, &timerid) == -1)
+    if (timer_create(CLOCK_REALTIME, &sev, &g_timer2) == -1)
     {
         perror("timer_create SIGEV_THREAD");
         exit(EXIT_FAILURE);
     }
-#endif
 
     // Start the timer
-    //its.it_value.tv_sec = 10;  // Initial expiration
-    //its.it_value.tv_nsec = 0;
-    //its.it_interval.tv_sec = 1;  // Interval for periodic timer
-    //its.it_interval.tv_nsec = 0;
     if (timer_settime(timerid, 0, &its, NULL) == -1)
     {
-        perror("timer_settime");
+        perror("timer_settime for timer1");
+        exit(EXIT_FAILURE);
+    }
+    its.it_value.tv_sec = INIT_SEC2;  // Initial expiration
+    its.it_value.tv_nsec = 0;
+    its.it_interval.tv_sec = INTV_SEC2;  // Interval for periodic timer
+    its.it_interval.tv_nsec = 0;
+    if (timer_settime(g_timer2, 0, &its, NULL) == -1)
+    {
+        perror("timer_settime for timer2");
         exit(EXIT_FAILURE);
     }
 
